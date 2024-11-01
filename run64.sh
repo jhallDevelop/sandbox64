f941b49c4cc8e7461358e7c45a169020c82eb76f
jhall.develop <jhall.develop@gmail.com>
2024-11-01 23:45:18 +1000

    ADDED: bash scripts to run on emu or console

---
+++ run64.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run UNFLoader with specified options
+~/Projects/n64/dev/N64-UNFLoader/UNFLoader/UNFLoader -r ~/Projects/AF/sandbox64/sandbox64.z64 -
---
+++ runAres.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run the Ares emulator with the specified ROM
+/usr/local/bin/ares.app/Contents/MacOS/ares sandbox64.z64
+
